d8a96f2de7b8b704638e918b64d7c954ed3f4f02
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-06-17 09:51:36 +0000

    Fixed the case: Insert left child in a root node that has no left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,5 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		newLeft->left = parent->left;
 		parent->left = newLeft;
 	}
+	else
+		praent->left = newLeft;
 	return (newLeft);
 }
