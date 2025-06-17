5fb55def6d51f39c76e22de1dd220ae5b2ae699b
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-06-17 10:41:22 +0000

    fixed betty coding style

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_delete - deletes an entire binary tree
- * @tree -  a pointer to the root node of the tree to delete
+ * @tree: a pointer to the root node of the tree to delete
  * Return: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
@@ -12,6 +12,4 @@ void binary_tree_delete(binary_tree_t *tree)
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 	free(tree);
-
-	return;
 }
